5dfae7a920b187030b572b4e252848b6fc81f814
JovinJovinsson <jovin.svein@gmail.com>
2022-06-21 14:38:05 +0800

    First version of the Painter.cs completed
    Some inefficiencies exist but it passes the 3 sample inputs on the problem page.

--- Coding-Challenges/Google Kick Start/2021-Round H/Painter.cs
+++ Coding-Challenges/Google Kick Start/2021-Round H/Painter.cs
@@ -20,41 +20,33 @@ class Painter
     {
         // Get the number of test cases
         int numCases = Convert.ToInt32(Console.ReadLine());
-        // Create a basic array for the primary colours
-        char[] primary = new char[] { 'R', 'Y', 'B' };
-        // Create a jagged array to store the values
-        char[][] secondary = new char['Q'][];
-        secondary['A'] = primary;
-        secondary['O'] = new char[] { 'R', 'Y' };
-        secondary['P'] = new char[] { 'R', 'B' };
-        secondary['G'] = new char[] { 'Y', 'B' };
-        // We'll leave U out of both as we can handle that directly
+        // Create a dictionary of the colours, leave out U as we just skip any spots with that
+        Dictionary<char, char[]> colours = new Dictionary<char, char[]>();
+        colours.Add('R', new char[] { 'R' });
+        colours.Add('Y', new char[] { 'Y' });
+        colours.Add('B', new char[] { 'B' });
+        colours.Add('O', new char[] { 'R', 'Y' });
+        colours.Add('P', new char[] { 'R', 'B' });
+        colours.Add('G', new char[] { 'B', 'Y' });
+        colours.Add('A', new char[] { 'R', 'Y', 'B' });
         // Repeat the code for the number of times
         for (int i = 0; i < numCases; i++)
         {
             // Retrieve the padlock & favourites strings as char[]
             int length = Convert.ToInt32(Console.ReadLine());
             char[] painting = (Console.ReadLine()).ToCharArray();
-            // The number of operations is the number of steps taken for the padlock to have letters matching the favourites
-            int numberOfStrokes = 0;
-            // Iterate over the characters of the painting
-            for (int j = 0; j < length; j++)
-            {
-                // This is our first stroke, ensure it's not uncoloured, then increment
-                if (j == 0 && painting[j] != 'U') { numberOfStrokes++; }
-                // Skip if it's uncoloured as we have no stroke, or if the stroke is the same as previous
-                else if (painting[j] == 'U' || painting[j] == painting[j - 1]) { continue; }
-                else
-                {
-                    if (Array.Exists(primary, element => element == painting[j]))
-                    {
-                        
-                    } else // We have a secondary colour
-                    {
 
-                    }
-                }
-            }
+            // Store the total number of strokes
+            int numberOfStrokes = 0;
+            
+            // Iterate over each colour
+            /*foreach (KeyValuePair<char, char[]> kvp in colours)
+            {*/
+            numberOfStrokes += strokesForColour(painting, length, 'R', colours);
+            numberOfStrokes += strokesForColour(painting, length, 'Y', colours);
+            numberOfStrokes += strokesForColour(painting, length, 'B', colours);
+            //}
+
             // Output the desired results
             Console.WriteLine("Case #" + (i + 1) + ": " + numberOfStrokes);
         }
@@ -62,5 +54,43 @@ class Painter
         Console.ReadKey();
     }
 
+    /// <summary>
+    /// Identifies the number of strokes required by unwrapping the colours to their base and ensuring no duplicate
+    /// </summary>
+    /// <param name="painting">The painting input as a char array</param>
+    /// <param name="length">The length of the painting</param>
+    /// <param name="colour">The current colour being analysed</param>
+    /// <param name="colourMap">The map of secondary colours to primary</param>
+    /// <returns></returns>
+    static int strokesForColour(char[] painting, int length, char colour, Dictionary<char, char[]> colourMap)
+    {
+        // Count the number of strokes for this colour
+        int strokes = 0;
+        // Store the position of of our stroke to ensure we don't double up
+        int lastStroke = int.MinValue;
+        
+        // Iterate over each position in the painting
+        for (int i = 0; i < length; i++)
+        {
+            // When it's uncoloured, just skip
+            if (painting[i] == 'U')
+            {
+                continue;
+            }
+            // If the current colour matches the input colour, or the colour is a parimary of the secondary colour
+            if (painting[i] == colour || Array.Exists(colourMap[painting[i]], element => element.Equals(colour)))
+            {
+                // If the last colour change position was not the previous position in the array, increment
+                if (lastStroke != i - 1)
+                {
+                    strokes++;
+                }
+                // We're matching the colour, so increase the stroke position
+                // This won't change if the colour changes
+                lastStroke = i;
+            }
+        }
 
+        return strokes;
+    }
 }
